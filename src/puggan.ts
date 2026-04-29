9400b3519548d2bba70052b50bc9c01932df84c9
Thorbjörn Puggan Sundragon <puggan@sundragon.se>
2026-04-29 21:40:03 +0200

    convert first script to ts

--- src/puggan.ts
+++ src/puggan.ts
@@ -1,5 +1,6 @@
+import { NS } from '@ns';
 /** @param {NS} ns */
-export async function main(ns) {
+export async function main(ns: NS) {
 	/*
 	for (const city of ['Sector-12', 'Aevum', 'Chongqing', 'New Tokyo', 'Ishima', 'Volhaven']) {
 		ns.tprint(
