74a59937c342b6d1ac026d275ed4a7e6ec3e3cca
Thorbjörn Puggan Sundragon <puggan@sundragon.se>
2026-04-29 22:01:23 +0200

    convert and bugfix watch.ts

--- src/startupCheep.js
+++ src/startupCheep.js
@@ -1,8 +1,8 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	ns.exec('scan.js', 'home', {threads: 1, preventDuplicates: true});
+	ns.exec('scan.ts', 'home', {threads: 1, preventDuplicates: true});
 	await ns.sleep(1000);
-	ns.exec('watch.js', 'home', {threads: 1, preventDuplicates: true}, 10, 'home', 'scan.js');
+	ns.exec('watch.js', 'home', {threads: 1, preventDuplicates: true}, 10, 'home', 'scan.ts');
 	//ns.exec('watch.js', 'home', {threads: 1, preventDuplicates: true}, 100, 'home', 'buyServers.js');
 	ns.exec('watch.js', 'home', {threads: 1, preventDuplicates: true}, 150, 'home', 'backdoorInstall.js');
 	ns.ui.openTail(ns.exec('gangCheep.js', 'home', {threads: 1, preventDuplicates: true}));
---
+++ src/watch.ts
@@ -0,0 +1,31 @@
+import {NS, AutocompleteData} from '@ns';
+
+/**
+ * @param {AutocompleteData} data
+ * @param {string[]} args
+ */
+export function autocomplete(data: AutocompleteData, args: string[]) {
+    switch (args.length) {
+        case 2:
+            return data.hostname;
+        case 3:
+            return data.scripts;
+        default:
+            return [];
+    }
+}
+
+/** @param {NS} ns */
+export async function main(ns: NS) {
+    const sec = parseFloat('' + ns.args[0]);
+    const host = '' + ns.args[1];
+    const script = '' + ns.args[2];
+    const scriptArgs = ns.args.slice(3);
+    while (true) {
+        const pid = ns.exec(script, host, 1, ...scriptArgs);
+        await ns.sleep(sec * 1000);
+        while (ns.isRunning(pid)) {
+            await ns.sleep(100);
+        }
+    }
+}
