96981256fd1d22e61506555f9a1d9d9969776f0b
Thorbjörn Puggan Sundragon <puggan@sundragon.se>
2026-04-30 22:26:00 +0200

    convert buyMaxServers.ts

---
+++ src/buyMaxServers.ts
@@ -0,0 +1,81 @@
+import {NS} from '@ns';
+
+/**
+ * @param {NS} ns
+ **/
+export async function main(ns: NS) {
+    const spareMoney = (ns.args.length > 0 ? +ns.args[0] : 1) * 1024 * 1024 * 1024;
+
+    const upgradeServer = async () => {
+        const maxRam = ns.getPurchasedServerMaxRam();
+        let worstServerHost: string | null = null;
+        let worstServerRam = maxRam - 1;
+        for (const serverName of ns.getPurchasedServers()) {
+            const serverRam = ns.getServerMaxRam(serverName);
+            if (serverRam >= worstServerRam) {
+                continue;
+            }
+            worstServerRam = serverRam;
+            worstServerHost = serverName
+        }
+        if (!worstServerHost) {
+            ns.tprint('Nothing to upgrade');
+            return false;
+        }
+
+        ns.tprintf('Server selected for upgrade %s with %s (of %s)', worstServerHost, ns.formatRam(worstServerRam), ns.formatRam(maxRam));
+
+        const cost = ns.getPurchasedServerUpgradeCost(worstServerHost, worstServerRam * 2);
+        let money = ns.getServerMoneyAvailable("home") - spareMoney;
+        if (money < cost) {
+            ns.tprint('Low Money');
+            ns.tprintf("%s + %s / %s", ns.formatNumber(spareMoney), ns.formatNumber(money), ns.formatNumber(cost))
+            return false;
+        }
+
+        ns.upgradePurchasedServer(worstServerHost, worstServerRam * 2);
+        ns.tprintf('Upgraded server %s -> %s', worstServerHost, ns.formatRam(worstServerRam * 2));
+        await ns.sleep(100);
+        return true;
+    };
+
+    const buyNewServer = async (ram: number) => {
+        const cost = ns.getPurchasedServerCost(ram);
+        let money = ns.getServerMoneyAvailable("home") - spareMoney;
+        if (money < cost) {
+            ns.tprint('Low Money');
+            ns.tprintf("%s + %s / %s", ns.formatNumber(spareMoney), ns.formatNumber(money), ns.formatNumber(cost))
+            return false;
+        }
+
+        const serverPrefix = 'myS';
+        const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ';
+        let suffixIndex = 0;
+        while (currentServers.indexOf(serverPrefix + alpha.charAt(suffixIndex)) >= 0 || ns.serverExists(serverPrefix + alpha.charAt(suffixIndex))) {
+            suffixIndex++;
+            await ns.sleep(100);
+        }
+        const serverName = serverPrefix + alpha.charAt(suffixIndex);
+        ns.purchaseServer(serverName, ram)
+        ns.tprint('New server: ' + serverName);
+        await ns.sleep(100);
+        return true;
+    };
+
+    const maxServers = ns.getPurchasedServerLimit();
+    const currentServers = ns.getPurchasedServers();
+
+    const homeRam = ns.getServerMaxRam('home');
+    const maxRam = ns.getPurchasedServerMaxRam();
+    let serverCount = currentServers.length;
+    while (serverCount < maxServers) {
+        if (await buyNewServer(Math.min(homeRam / 2, maxRam / 4))) {
+            serverCount++;
+        } else {
+            return;
+        }
+    }
+
+    while (await upgradeServer()) {
+    }
+}
