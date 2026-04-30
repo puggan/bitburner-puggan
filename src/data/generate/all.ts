a41ecf33544afb8ac76f656b4e4ea3030467f886
Thorbjörn Puggan Sundragon <puggan@sundragon.se>
2026-05-01 01:48:51 +0200

    generater all script

---
+++ src/data/generate/all.ts
@@ -0,0 +1,30 @@
+import type {NS} from '@ns';
+
+async function waitForPid(ns: NS, pid: number) {
+    while (ns.isRunning(pid)) {
+        await ns.sleep(100);
+    }
+}
+
+export async function runAll(ns: NS)
+{
+    const scripts = [
+        '/data/generate/serverNames.js',
+        '/data/generate/servers.js',
+    ];
+    for (const script of scripts) {
+        const pid = ns.run(script);
+
+        if (!pid) {
+            ns.tprint(`ERROR: Failed to start ${script}. Possibly not enough RAM?`);
+            return;
+        }
+
+        await waitForPid(ns, pid);
+    }
+}
+
+export async function main(ns: NS)
+{
+    await runAll(ns);
+}
