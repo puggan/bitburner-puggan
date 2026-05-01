d6a0196edffeb25fb19c9906ef8e39ac7e501d82
Thorbjörn Puggan Sundragon <puggan@sundragon.se>
2026-05-01 10:16:21 +0200

    repair player, missing properties

--- src/data/generate/player.ts
+++ src/data/generate/player.ts
@@ -1,17 +1,22 @@
-import {NS} from '@ns';
+import {NS, Player} from '@ns';
 
 /** @param {NS} ns */
 export function main(ns: NS) {
     const fullPlayer = ns.getPlayer();
-    const limitedPlayer = {
-        money: fullPlayer.money,
-        numPeopleKilled: fullPlayer.numPeopleKilled,
+    const limitedPlayer: Player = {
+        city: fullPlayer.city,
         entropy: fullPlayer.entropy,
-        jobs: fullPlayer.jobs,
+        exp: fullPlayer.exp,
         factions: fullPlayer.factions,
-        totalPlaytime: fullPlayer.totalPlaytime,
-        location: fullPlayer.location,
+        hp: fullPlayer.hp,
+        jobs: fullPlayer.jobs,
         karma: fullPlayer.karma,
+        location: fullPlayer.location,
+        money: fullPlayer.money,
+        mults: fullPlayer.mults,
+        numPeopleKilled: fullPlayer.numPeopleKilled,
+        skills: fullPlayer.skills,
+        totalPlaytime: fullPlayer.totalPlaytime,
     };
     ns.write('data/player.json.txt', JSON.stringify(limitedPlayer, null, 2), 'w');
 }
